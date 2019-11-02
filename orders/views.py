7951d2efec9ae4c24cc052e3e03d741c974ee6c4
O Barron <otsobnalle@gmail.com>
2019-11-01 21:16:38 -0700

    Added login and logout using premade DJANGO assets. I see now how useful this is.

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,30 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    context = {
+        "user": request.user
+    }
+    return render(request, "orders/index.html", context)
+
+def login_view(request):
+    if request.method == 'GET':
+        return render(request, "orders/login.html")
+
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "orders/login.html", {"message": "Invalid credentials."})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
