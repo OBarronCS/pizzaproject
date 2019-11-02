99ba9ff7a341d3d0271cdaae776cb4f21167abad
O Barron <otsobnalle@gmail.com>
2019-11-01 22:17:38 -0700

    User registration is live! They can input their names, a username, a password, and an email, and login with those credentials later!

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from .forms import SignUpForm
 
 # Create your views here.
 def index(request):
@@ -13,6 +15,11 @@ def index(request):
     return render(request, "orders/index.html", context)
 
 def login_view(request):
+    # IF USER IS already logged in, redirect home
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("index"))
+
+    # if this getting the form data,
     if request.method == 'GET':
         return render(request, "orders/login.html")
 
@@ -28,3 +35,18 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("login"))
+
+def register_view(request):
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        form = SignUpForm()
+
+    return render(request,'orders/register.html', {'form':form})
+    #username, password, first name, last name, and email address.
